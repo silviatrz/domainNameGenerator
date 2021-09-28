2fd25b6455d571d27898627a4ace05da26a6a8fd
Silvia <silviatmnez@gmail.com>
2021-09-28 20:11:46 +0000

    updated index.html and app.js with domain name generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our", "your", "their", "a"];
+let adj = ["great", "big", "amazing", "awesome", "cool"];
+let noun = ["jogger", "racoon", "dolphin", "fox", "platypus"];
+let extension = [".com", ".net", ".es", ".us", ".io"];
+
+const generateDomain = (pronoun, adj, noun, extension) => {
+  let domain = "";
+
+  pronoun.forEach(p => {
+    adj.forEach(a => {
+      noun.forEach(n => {
+        extension.forEach(e => {
+          domain += "<li>" + p + a + n + e + "</li>";
+        });
+      });
+    });
+  });
+
+  return domain;
 };
+
+//for (let p in pronoun) {
+//domain = p;
+//}
+console.log(generateDomain(pronoun, adj, noun, extension));
+document.querySelector("#domains").innerHTML = generateDomain(
+  pronoun,
+  adj,
+  noun,
+  extension
+);
